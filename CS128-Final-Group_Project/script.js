d7567efe1747d8ec36c641470f84578674dcd5e4
Khaos <ktree195@gmail.com>
2026-06-18 00:55:09 +0700

    minor edit

--- CS128-Final-Group_Project/script.js
+++ CS128-Final-Group_Project/script.js
@@ -70,3 +70,102 @@ function pauseTimer() {
     clearInterval(timerId);
     timerId = null;
 }
+
+function resetTimer() {
+    pauseTimer();
+    isCurrentlyStudy = true;
+    timeLeft = study_time;
+    statusLabel, textContent = gif_study;
+    updateDisplay;
+}
+
+function switchMode() {
+    if (isCurrentlyStudy) {
+        // earn 2 xp when finished a full session
+        affection_xp += 2;
+        let newLevel = Math.floor(affection_xp / XP_per_Level) + 1;
+        if (newLevel > affection_level) {
+            affection_level = newLevel;
+            triggerAchievementNotification(`Level UP! Level ${affection_level} Unlocked!`);
+        }
+        localStorage.setItem('companionXP', affection_xp);
+        localStorage.setItem('companionLevel', affection_level);
+        updateStatusUI();
+
+        isCurrentlyStudy = false;
+        timeLeft = break_time;
+        statusLabel.textContent = "Hard Work deserve a break.";
+        characterImg.src = gif_break;
+    } else {
+        isCurrentlyStudy = true;
+        timeLeft = study_time;
+        statusLabel.textContent = "Let continue where we left off.";
+        characterImg.src = gif_study;
+    }
+    updateDisplay();
+    startTimer();
+}
+
+// tap/click action
+
+function pokeCharacter() {
+    if (isPoked) return;
+    isPoked = true;
+
+    let dynamicPhrases = dialouge_tier.level1;
+    if (affection_level >= 5) {
+        dynamicPhrases = dialouge_tier.level5;
+    } else if (affection_level >= 3) {
+        dynamicPhrases = dialouge_tier.level3;
+    }
+    const randomIndex = Math.floor(Math.random() * dynamicPhrases.length);
+    bubble.textContent = dynamicPhrases[randomIndex];
+
+    bubble.classList.remove('hidden-bubble');
+    characterImg.src = gif_poked;
+
+    setTimeout(() => {
+        bubble.classList.add('hidden-bubble');
+        characterImg.src = isCurrentlyStudy ? gif_study : gif_break;
+        isPoked = false;
+    }, 2000);
+}
+
+// achivementn logic
+
+function triggerAchievementNotification(message) {
+    achievementBox.textContent = message;
+    achievementBox.style.display = "block";
+    setTimeout(() => {
+        achievementBox.style.display = "none";
+    }, 5000);
+}
+
+function checkMilestoneBadge() {
+    if (affection_level === 3 && !localStorage.getItem('badge_lv3')) {
+        localStorage.getItem('badge_lv3', true);
+        triggerAchievementNotification("Achievement: Soften Heart");
+    }
+    if (affection_level === 5 && !localStorage.getItem('badge_lv5')) {
+        localStorage.getItem('badge_lv5', true);
+        triggerAchievementNotification("Achievement: Trusted Friend");
+    }
+}
+
+// toggle text changes with bootstraps
+function toggleMusicButtonText() {
+    const toggleBtn = document.getElementById('toggle-music-btn');
+    //time delay for double check
+    setTimeout(() => {
+        const musicPanel = document.getElementById('musicCollapse');
+        if (musicPanel.classList.contains('show')) {
+            toggleBtn.textContent = "Hide Music Panel";
+        } else {
+            toggleBtn.textContent = "Show Music Panel";
+        }
+    }, 50);
+}
+
+// data ui sync + execution?
+updateDisplay();
+updateStatusUI;
